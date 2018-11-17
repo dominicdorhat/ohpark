0fff2ff910907b0bcc7bc7b862fcf6b5414433c9
Hong <khfy6hzy@nottingham.edu.my>
2018-11-17 14:33:18 +0800

    asset for login page and menu component

---
+++ Components/Menu.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import {ImagePicker , Permissions} from 'expo';
+
+const url = "https://lpr.recoqnitics.com/detect"
+const access_key = "8044c46d33a99d066ace"
+const secret_key = "4705011ce68297849185e18c15ab2413d1019ebc"
+
+
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isBooked : true,
+    }
+  }
+
+  static navigationOptions = {
+    title: 'Welcome'
+  };
+  
+  __gotoUnlock = () => {
+    console.log("Unlock UI");
+  }
+
+  __gotoRegister = () => {
+    this.uploadCarPlatePicture();
+  }
+
+  uploadCarPlatePicture = async() => {
+    var formData = new FormData();
+
+    //manually get permissions
+    const {
+      status: cameraPerm
+    } = await Permissions.askAsync(Permissions.CAMERA);
+
+    const {
+      status: cameraRollPerm
+     } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+
+    if(cameraPerm === 'granted' && cameraRollPerm === 'granted'){
+      let result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+      })
+
+      console.log(result)
+
+      var image = {
+        uri: result.uri,
+        type: 'image/jpg',
+        name: 'image.jpg',
+      }
+
+      formData.append("filename",image)
+      formData.append("access_key",access_key)
+      formData.append("secret_key",secret_key)
+
+      fetch(url, {
+        method: 'POST',
+        body: formData
+      })
+        .then(response => response.json())
+        .then(json => console.log(json))
+    }     
+
+  }
+
+  render() {
+    const showUnlock = <Button title = "Unlock car park" onPress = {this.__gotoUnlock}></Button>;
+    const { navigate } = this.props.navigation;
+
+    return (
+      <View style={styles.container}>
+        <Button title = "Book a car park" onPress = {() => navigate('RentPark')}/>      
+        <View>
+          {this.state.isBooked ? showUnlock: null}
+        </View>
+        <Button title="Register car plate" onPress={this.__gotoRegister} />      
+      </View>
+    );
+  }
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Menu
+
+
