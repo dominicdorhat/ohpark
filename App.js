903e0896bd9ae3d6ee40dba40ab078a059f1a92e
wxyooong <wongxingyoong@hotmail.com>
2018-11-17 19:47:39 +0800

    move logo to middle

--- App.js
+++ App.js
@@ -19,39 +19,51 @@ class Login extends React.Component {
 
   render() {
     return (
-      <KeyboardAvoidingView style={styles.container} enabled>
+      <View style={styles.container}>
         <Image source={Icon}/>
         <Text>Oh Park !!!</Text>
         <View style={styles.row}>
-          <Text>Phone No:</Text>
-          <TextInput 
-              placeholder = "phone number"
-              placeholderTextColor = 'rgba(0,122,0,0.5)'
-              returnKeyType = 'next'
-              onChangeText={ (x)=>this.setState({phone_no:x}) }
+          <Text>Phone No. :</Text>
+          <TextInput
+            placeholder = "eg. 012337... "
+            placeholderTextColor = 'rgba(0,122,0,0.5)'
+            returnKeyType = 'next'
+            onChangeText={ (x)=>this.setState({phone_no:x}) }
           />
         </View>
-        <View style={styles.container}>
-          <Button title="GO!" onPress={this.__gotoMenu}/>
+        <View style={styles.row1}>
+          <Button title="GO!"/>
         </View>
-      </KeyboardAvoidingView>
+      </View>
     );
   }
 }
-
+//  <View style={styles.row}>
+//           <Text>Phone No:</Text>
+//           <TextInput 
+//               placeholder = "phone number"
+//               placeholderTextColor = 'rgba(0,122,0,0.5)'
+//               returnKeyType = 'next'
+//               onChangeText={ (x)=>this.setState({phone_no:x}) }
+//           />
+//         </View>
+//         <View style={styles.container}>
+//           <Button title="GO!" onPress={this.__gotoMenu}/>
+//         </View>
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    paddingTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   row: {
+    marginTop: 30,
+    backgroundColor: '#fff',
     flexDirection: 'row',
   },
   row1: {
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 40
   },
 });
 
